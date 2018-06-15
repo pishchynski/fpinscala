00ea11fcc5d38bef881c420552ceca5218a1f724
pishchynski <pavelalexbel@outlook.com>
2018-06-15 18:01:19 +0300

    Ex2_3. Implemented curry method

---
+++ src/main/scala/exercise2_3/Currier.scala
@@ -0,0 +1,12 @@
+package exercise2_3
+
+/**
+  * Let’s look at another example, currying, which converts a function f of two arguments
+  * into a function of one argument that partially applies f. Here again there’s only one
+  * implementation that compiles. Write this implementation
+  */
+object Currier {
+  def curry[A, B, C](f: (A, B) => C): A => B => C = {
+    a: A => (b: B) => f(a, b)
+  }
+}
